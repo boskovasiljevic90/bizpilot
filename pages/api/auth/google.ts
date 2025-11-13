0792084bf48c03499e5bae7bb7c60a3f02c8236e
boskovasiljevic90 <boskovasiljevic90@gmail.com>
2025-11-13 21:36:29 +0100

    Update google.ts

--- pages/api/auth/google.ts
+++ pages/api/auth/google.ts
@@ -1,14 +1,15 @@
 import { google } from "googleapis";
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    "https://yourdomain.com/api/auth/callback"
+    process.env.GOOGLE_CLIENT_ID!,
+    process.env.GOOGLE_CLIENT_SECRET!,
+    `${req.headers.origin}/api/auth/callback`
   );
 
   const url = oauth2Client.generateAuthUrl({
     access_type: "offline",
+    prompt: "consent",
     scope: ["https://www.googleapis.com/auth/business.manage"],
   });
 
