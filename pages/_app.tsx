e1ea4e5d1fc0d3d2bebb6b1cca47483e60510e05
boskovasiljevic90 <boskovasiljevic90@gmail.com>
2025-12-16 21:06:10 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={(pageProps as any)?.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
